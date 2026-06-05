4f411fc4e8d98931e334cce4d4dd40ce154b615c
rin <rin@rinne.dev>
2026-06-06 01:37:03 +0200

    broken opengl context

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+APP_NAME=Fuku
+XWIN_DIR=~/xwin
+
+cd "$(dirname -- "${BASH_SOURCE[0]}")"
+
+mkdir -p build
+cd build
+
+CROSS_COMPILE_OPTIONS=(-fuse-ld=lld /vctoolsdir $XWIN_DIR/crt /winsdkdir $XWIN_DIR/sdk)
+WARN_OPTIONS=(
+	-Wall
+	-Wextra
+	-Wshadow
+	-Wno-missing-prototypes
+	-Wno-missing-variable-declarations
+	-Wno-declaration-after-statement
+	-Wno-unused-parameter
+	-Wno-extra-semi-stmt
+	-Wno-cast-qual
+	-Wno-unsafe-buffer-usage
+)
+
+InvalidArguments() {
+	echo "Invalid Arguments. Usage: build.sh [debug | release] [all | platform | game]"
+	exit 1
+}
+
+COMPILE_OPTIONS=(${CROSS_COMPILE_OPTIONS[@]} ${WARN_OPTIONS[@]} /DAPP_NAME=\"$APP_NAME\" /I../vendor)
+
+if [[ $1 == "debug" ]]; then
+	COMPILE_OPTIONS=(${COMPILE_OPTIONS[@]} -g -Od )
+elif [[ $1 == "release" ]]; then
+	COMPILE_OPTIONS=(${COMPILE_OPTIONS[@]} -O2 )
+else
+	InvalidArguments
+fi
+
+BUILD_PLATFORM=0
+BUILD_GAME=0
+
+if [[ $2 == "all" ]]; then
+	BUILD_PLATFORM=1
+	BUILD_GAME=1
+elif [[ $2 == "platform" ]]; then
+	BUILD_PLATFORM=1
+	BUILD_GAME=0
+elif [[ $2 == "game" ]]; then
+	BUILD_PLATFORM=0
+	BUILD_GAME=1
+else
+	InvalidArguments
+fi
+
+if [[ $BUILD_PLATFORM != 0 ]]; then
+	clang-cl ${COMPILE_OPTIONS[@]} ../src/platform.c user32.lib gdi32.lib opengl32.lib /link /incremental:no /out:$APP_NAME.exe
+fi
---
+++ src/common.h
@@ -0,0 +1,68 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+#define S8_MIN  ( (s8)0x80)
+#define S16_MIN ((s16)0x8000)
+#define S32_MIN ((s32)0x80000000)
+#define S64_MIN ((s32)0x8000000000000000LL)
+
+#define S8_MAX  ( (s8)0x7F)
+#define S16_MAX ((s16)0x7FFF)
+#define S32_MAX ((s32)0x7FFFFFFF)
+#define S64_MAX ((s32)0x7FFFFFFFFFFFFFFFLL)
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#define U8_MAX  ( (u8)0xFFU)
+#define U16_MAX ((u16)0xFFFFU)
+#define U32_MAX ((u32)0xFFFFFFFFU)
+#define U64_MAX ((u64)0xFFFFFFFFFFFFFFFFULL)
+
+typedef s64 smm;
+typedef u64 umm;
+
+#define SMM_MIN S64_MIN
+#define SMM_MAX S64_MAX
+#define UMM_MAX U64_MAX
+
+typedef float f32;
+typedef double f64;
+
+typedef struct String
+{
+	u8* data;
+	u64 len;
+} String;
+
+#define STRING(S) (String){ .data = (u8*)(S), .len = sizeof(S)-1 }
+
+#define ARRAY_LEN(A) (sizeof(A)/sizeof(0[A]))
+
+#define CONCAT__(A, B) A##B
+#define CONCAT_(A, B) CONCAT__(A, B)
+#define CONCAT(A, B) CONCAT_(A, B)
+
+#define ASSERT(EX) ((EX) ? (1) : ((*(volatile int*)0 = 0), 0))
+#define STATIC_ASSERT(EX, MSG) static_assert((EX), MSG)
+
+#define NOT_IMPLEMENTED ASSERT("NOT_IMPLEMENTED" != 0)
+
+#define BREAK_IF(E) { if (E) break; }
+
+inline bool
+String_Equal(String a, String b)
+{
+	bool result = (a.len == b.len);
+
+	for (umm i = 0; i < a.len && result; ++i) result = (a.data[i] == b.data[i]);
+
+	return result;
+}
